2a835f1fbd8c8b74171fad5144679931b34169f3
MarcusKri <marcus-kristoffersen@outlook.com>
2021-10-13 09:55:14 +0200

    shouldRetrieveSavedPersonFromDatabase should retrieve save person from database (PASSED)

--- src/main/java/no/kristiania/person/PersonDao.java
+++ src/main/java/no/kristiania/person/PersonDao.java
@@ -1,9 +1,7 @@
 package no.kristiania.person;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PersonDao {
 
@@ -18,12 +16,18 @@ public class PersonDao {
         try (Connection connection = dataSource.getConnection()) {
 
            try (PreparedStatement statement = connection.prepareStatement(
-                    "insert into people (first_name, last_name) values (?, ?)"
+                    "insert into people (first_name, last_name) values (?, ?)",
+                   Statement.RETURN_GENERATED_KEYS
             )) {
-                statement.setString(1, person.getFirstName());
+               statement.setString(1, person.getFirstName());
                statement.setString(2, person.getLastName());
 
                statement.executeUpdate();
+
+               try (ResultSet rs = statement.getGeneratedKeys()) {
+                   rs.next();
+                   person.setId(rs.getLong("id"));
+               }
            }
         }
         this.person = person;
@@ -31,7 +35,25 @@ public class PersonDao {
 
     //Long id kan settes til = null
     //long kan ikke
-    public Object retrieve(Long id) {
-        return person;
+    public Person retrieve(long id) throws SQLException {
+
+        try (Connection connection = dataSource.getConnection()) {
+
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from people where id = ?"
+            )) {
+                statement.setLong(1, id);
+
+                try (ResultSet rs = statement.executeQuery()) {
+                    rs.next();
+
+                    Person person = new Person();
+                    person.setId(rs.getLong("id"));
+                    person.setFirstName(rs.getString("first_name"));
+                    person.setLastName(rs.getString("last_name"));
+                    return person;
+                }
+            }
+        }
     }
 }
